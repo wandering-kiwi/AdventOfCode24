03a309523aede5ef8cf5d9200200d9aa13db4988
wandering-kiwi <67548758+wandering-kiwi@users.noreply.github.com>
2024-12-01 18:25:17 +0000

    Update part1.py

--- day1/part1.py
+++ day1/part1.py
@@ -1,1 +1,1 @@
-print('hello world')
+print(sum((lambda y: [abs(y[1][j]- y[0][j]) for j in range(len(y[0]))])((lambda x: [sorted(x[::2]), sorted(x[1::2])])([int(i) for i in open("input.txt", "r").read().split()]))))
